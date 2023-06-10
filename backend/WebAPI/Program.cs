8ba05ad0ab9419d16796d4a32ed7409e5db03966
Alex Horban <sahok6264@gmail.com>
2023-06-10 08:39:36 +0300

    Refactor CORS policy and add connection string handling
    - Refactored the CORS policy to allow any header, method, and credentials
    - Added support for reading connection strings from environment variables
    - Printed the current connection string to console for debugging purposes

--- backend/WebAPI/Program.cs
+++ backend/WebAPI/Program.cs
@@ -17,52 +17,63 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddCors(options =>
 {
-	options.AddPolicy(name: angularCorsPolicy, builder =>
-	{
-		builder
-			.AllowAnyHeader()
-			.AllowAnyMethod()
-			.AllowCredentials()
-			.WithOrigins("http://localhost:4200", "http://localhost:5000", "https://localhost:5001");
-	});
+    options.AddPolicy(name: angularCorsPolicy, builder =>
+    {
+        builder
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .WithOrigins("http://localhost:4200", "http://localhost:5000", "https://localhost:5001");
+    });
 });
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 builder.Services.AddSwaggerGen(c =>
 {
-	c.SwaggerDoc("v1", new OpenApiInfo
-	{
-		Version = "v1",
-		Title = "WebApi",
-	});
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "WebApi",
+    });
 });
 
+string connection;
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddEnvironmentVariables().AddJsonFile("appsettings.Development.json");
+    connection = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+else
+{
+    connection = Environment.GetEnvironmentVariable("DefaultConnection");
+}
+Console.WriteLine($"Current connection string is: {connection}");
+
 builder.Services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-Console.WriteLine($"Current connection string is: {builder.Configuration.GetConnectionString("DefaultConnection")}");
+    options.UseSqlServer(connection));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 else
 {
-	app.UseDeveloperExceptionPage();
+    app.UseDeveloperExceptionPage();
 }
 
 using (var scope = app.Services.CreateScope())
 {
-	var services = scope.ServiceProvider;
+    var services = scope.ServiceProvider;
 
-	var context = services.GetRequiredService<ApplicationDbContext>();
-	context.Database.EnsureCreated();
-	if (context.Database.GetPendingMigrations().Any())
-	{
-		context.Database.Migrate();
-	}
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.EnsureCreated();
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
 }
 
 app.UseHttpsRedirection();
